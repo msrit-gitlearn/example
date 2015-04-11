a3e851ad111c93def80491da1049b31d7979815d
msrit-gitlearn <kartik.gayatri@yahoo.in>
2015-04-11 17:23:42 +0530

    adding comments

--- calc.py
+++ calc.py
@@ -1,12 +1,16 @@
+# to add two numbers
 def add(a, b):
 	return a + b
 
+# to subtract two numbers
 def subtract(a, b):
 	return a - b
 
+# to multiply two numbers
 def multiply(a, b):
 	return a * b
 
+# to divide two numbers
 def divide(a, b):
 	return a / b
 
